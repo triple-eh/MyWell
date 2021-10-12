731d6158f1be69cdba04ddd14186a199c13b4c17
Alex Aleksandrov <aleckaleksandrov@gmail.com>
2021-10-11 21:07:29 -0700

    created tests for JournalEntry

--- src/main/model/JournalEntry.java
+++ src/main/model/JournalEntry.java
@@ -1,13 +1,13 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 //A single entry in the journal
 public class JournalEntry {
-    private Date date;
+    private LocalDate date;
     private String overallState;
     private List<Sensation> sensations;
     private List<Feeling> feelings;
@@ -22,29 +22,43 @@ public class JournalEntry {
 
     //REQUIRES a valid date not greater than today's date and overallState be one of STATES
     //EFFECTS creates a new journal entry with specified date and specified overall state
-    public JournalEntry(Date date, String overallState) {
+    public JournalEntry(LocalDate date, String overallState) {
 
     }
 
     //getters
-    public Date getDate() {
+    public LocalDate getDate() {
+
         return this.date;
     }
 
     public String getOverallState() {
+
         return this.overallState;
+    }
+
+    public List<Sensation> getSensations() {
+
+        return new ArrayList<>();
+
+    }
+
+    public List<Feeling> getFeelings() {
+
+        return new ArrayList<>();
+
     }
 
     //EFFECTS adds sensation to the list of sensations if associated body part
     //        doesn't already have a sensation in the list, and returns true
     //        otherwise returns false
-    public boolean addPhysicalSensation(Sensation sensation) {
+    public boolean addSensation(Sensation sensation) {
         return false;
     }
 
     //EFFECTS adds feeling to the list of feelings if not already in the list, and returns true
     //        otherwise returns false
-    public boolean addFeeling() {
+    public boolean addFeeling(Feeling feeling) {
         return false;
     }
 
--- src/test/model/JournalEntryTest.java
+++ src/test/model/JournalEntryTest.java
@@ -1,4 +1,57 @@
 package model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 public class JournalEntryTest {
+
+    @Test
+    public void journalEntryTestWithDefaultDate() {
+        LocalDate today = LocalDate.now();
+        JournalEntry entryA = new JournalEntry("terrible");
+        Assertions.assertEquals("terrible",entryA.getOverallState());
+        Assertions.assertEquals(today, entryA.getDate());
+        JournalEntry entryB = new JournalEntry("bad");
+        Assertions.assertEquals("bad",entryB.getOverallState());
+    }
+
+    @Test
+    public void journalEntryTestWithSetDate() {
+        LocalDate date1 = LocalDate.of(2021,10,1);
+        LocalDate date2 = LocalDate.of(2021,10,2);
+        JournalEntry entryA = new JournalEntry(date1,"good");
+        Assertions.assertEquals(date1, entryA.getDate());
+        JournalEntry entryB = new JournalEntry(date2,"amazing");
+        Assertions.assertEquals(date2, entryA.getDate());
+    }
+
+    @Test
+    public void addSensationTest() {
+        JournalEntry entryA = new JournalEntry("good");
+        Assertions.assertEquals(0,entryA.getSensations().size());
+        Sensation sensation1 = new Sensation("left arm");
+        Sensation sensation2 = new Sensation("right arm");
+        entryA.addSensation(sensation1);
+        Assertions.assertEquals(1,entryA.getSensations().size());
+        entryA.addSensation(sensation2);
+        Assertions.assertEquals(2,entryA.getSensations().size());
+        Assertions.assertEquals(sensation1, entryA.getSensations().get(0));
+        Assertions.assertEquals(sensation2, entryA.getSensations().get(1));
+    }
+
+    @Test
+    public void addFeelingTest() {
+        JournalEntry entryA = new JournalEntry("good");
+        Assertions.assertEquals(0,entryA.getFeelings().size());
+        Feeling feeling1 = new Feeling("elated");
+        Feeling feeling2 = new Feeling("anxious");
+        entryA.addFeeling(feeling1);
+        Assertions.assertEquals(1,entryA.getFeelings().size());
+        entryA.addFeeling(feeling2);
+        Assertions.assertEquals(2,entryA.getFeelings().size());
+        Assertions.assertEquals(feeling1, entryA.getFeelings().get(0));
+        Assertions.assertEquals(feeling2, entryA.getFeelings().get(1));
+    }
 }

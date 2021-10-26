5417b2d1466b3639d9409eab584002587553ffb4
Alex Aleksandrov <aleckaleksandrov@gmail.com>
2021-10-25 21:57:16 -0700

    refactored tests

--- src/test/model/SensationTest.java
+++ src/test/model/SensationTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 public class SensationTest {
     Sensation s1;
     Sensation s2;
+    Sensation s3;
 
     @BeforeEach
     public void setup () {
         s1 = new Sensation("right arm");
         s2 = new Sensation("head");
+        s3 = new Sensation("right arm");
     }
 
     @Test
@@ -28,5 +30,54 @@ public class SensationTest {
         Assertions.assertEquals("unpleasant",s2.getSensationType());
         s2.setSensationType("neutral");
         Assertions.assertEquals("neutral",s2.getSensationType());
+    }
+
+    @Test
+    public void equalsTest() {
+        Assertions.assertFalse(s1.equals(null));
+        Assertions.assertFalse(s1.equals(new Feeling("right arm")));
+        Assertions.assertTrue(s1.equals(s3));
+        Assertions.assertFalse(s1.equals(s2));
+        s1.setSensationType("pleasant");
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setSensationType("unpleasant");
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setSensationType("pleasant");
+        Assertions.assertTrue(s1.equals(s3));
+        s1.setNote("note");
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setNote("other note");
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setNote("note");
+        Assertions.assertTrue(s1.equals(s3));
+        s1.setIntensity(5);
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setIntensity(4);
+        Assertions.assertFalse(s1.equals(s3));
+        s3.setIntensity(5);
+        Assertions.assertTrue(s1.equals(s3));
+    }
+
+    @Test
+    public void hashCodeTest() {
+        Assertions.assertNotEquals(s1.hashCode(), s2.hashCode());
+        Assertions.assertEquals(s1.hashCode(), s3.hashCode());
+        s1.setSensationType("pleasant");
+        Assertions.assertNotEquals(s1.hashCode(), s3.hashCode());
+        s3.setSensationType("pleasant");
+        Assertions.assertEquals(s1.hashCode(), s3.hashCode());
+        s1.setIntensity(5);
+        Assertions.assertNotEquals(s1.hashCode(), s3.hashCode());
+        s3.setIntensity(4);
+        Assertions.assertNotEquals(s1.hashCode(), s3.hashCode());
+        s3.setIntensity(5);
+        Assertions.assertEquals(s1.hashCode(), s3.hashCode());
+
+        s1.setNote("note");
+        Assertions.assertNotEquals(s1.hashCode(), s3.hashCode());
+        s3.setNote("not a note");
+        Assertions.assertNotEquals(s1.hashCode(), s3.hashCode());
+        s3.setNote("note");
+        Assertions.assertEquals(s1.hashCode(), s3.hashCode());
     }
 }
